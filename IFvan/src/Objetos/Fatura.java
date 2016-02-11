98bfe87dc589586ca497f738f650ee4cd7f437c2
AtFortunato <athus.sm@gmail.com>
2016-02-11 03:01:15 -0300

    Update Fatura.java

---
+++ IFvan/src/Objetos/Fatura.java
@@ -0,0 +1,34 @@
+package Objetos;
+
+public class Fatura {
+	private int cpf;
+	private String nome;
+	private String tipo;
+	private int lugar;
+	
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getLugar() {
+		return lugar;
+	}
+	public void setLugar(int lugar) {
+		this.lugar = lugar;
+	}
+	
+}
