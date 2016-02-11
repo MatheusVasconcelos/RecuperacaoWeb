7fa6548a507ae6ad12472393916f97679f1566ff
MatzCouz <matheus.sousacoutinho@gmail.com>
2016-02-11 03:03:33 -0300

    Update Usuario.java

---
+++ IFvan/src/Objetos/Usuario.java
@@ -0,0 +1,48 @@
+package Objetos;
+
+public class Usuario {
+	private String nome;
+	private String idade;
+	private String cpf;
+	private String sexo;
+	private String ocupacao;
+	private String senha;
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIdade() {
+		return idade;
+	}
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getOcupacao() {
+		return ocupacao;
+	}
+	public void setOcupacao(String ocupacao) {
+		this.ocupacao = ocupacao;
+	}
+	
+}
